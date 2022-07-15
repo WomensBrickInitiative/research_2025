655828486dce73275c8a1fcec7e1adafb724fdd7
yzhang18 <yzhang18@bonny.smith.edu>
2022-07-15 16:17:31 -0400

    new screentime data, interactive plot, screentime v.s. count in minifigs and characters

--- minifigs/starwars/films_screentime.R
+++ minifigs/starwars/films_screentime.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 source(here::here("wbi_colors.R"))
 library(rvest)
-
+library(plotly)
 # pull screentime data for films
 url <- "https://www.imdb.com/list/ls027631145/"
 robotstxt::paths_allowed(paths = c(url))
@@ -36,3 +36,79 @@ dummy <- time_summarized |>
   select(name, seconds, minutes, gender, species)
 
 write_csv(dummy, "starwars_screentime.csv")
+
+## Data filled in from Thorin and Alexander
+screentime <- read_csv(here::here("data", "starwars_screentime.csv"))
+screentime <- screentime |>
+  select(-notes) |>
+  mutate(species=tolower(species),is_human=ifelse(species=="human","human","non human")) |>
+  mutate(name_detect = case_when(name_detect=="BB-8"~"BB-8 ",
+                   name_detect=="Sabe"~"Sabe ",
+                   name_detect=="Sebulba"~"Sebulba ",
+                   name_detect=="Val"~"Val ",
+                   TRUE ~ name_detect))
+
+minifigs <- read_csv(here::here("data", "minifigs_data.csv"))
+starwars <- minifigs |>
+  filter(category=="Star Wars")
+
+# compute number of minifigs for each character
+minifig_characters <- tolower(starwars$description)
+movie_characters <- tolower(screentime$name_detect)
+
+num_minifigs <- map_int(movie_characters, ~sum(str_detect(minifig_characters, .x)))
+
+screentime <- screentime |>
+  mutate(num_minifigs=num_minifigs) |>
+  # combine anakin skywalker and darth vader
+  mutate(num_minifigs=ifelse(name=="Anakin Skywalker / Darth Vader",53,num_minifigs)) |>
+  select(-name_detect) |>
+  distinct() |>
+  mutate(has_minifigs=ifelse(num_minifigs>0,TRUE,FALSE))
+
+# Interactive scatter plot of #minifigs over screentime (min)
+p <- ggplotly(
+  ggplot(screentime, aes(x=minutes, y=num_minifigs, color=gender,text = paste("Name:", name, "\n#Minifigs:", num_minifigs,"\nScreentime:", minutes, "\nSpecies:", species))) +
+  geom_point() +
+  scale_color_wbi() +
+  labs(title = "Relationship between Screentime and Number of Star Wars Minifigs",
+       x = "Screentime (minutes)",
+       y = "Number of Minifigs",
+       color = "Gender"),
+  tooltip = "text"
+)
+p
+
+# dataset for making bar chart comparing screentime by gender
+screentime_summarized <- screentime |>
+  select(gender, num_minifigs, minutes) |>
+  group_by(gender) |>
+  summarize(count_gender=n(), time_gender=sum(minutes), count_minifigs=sum(num_minifigs)) |>
+  filter(gender!="neutral") |>
+  mutate(screentime_hours=round(time_gender/60,2)) |>
+  pivot_longer(cols = c(count_gender, count_minifigs, screentime_hours), names_to = "type", values_to = "value")
+
+# dataset for making bar chart comparing minifig&character count by gender
+count_summarized <- screentime_summarized |>
+  filter(type!="screentime_hours")
+
+# Bar chart to compare minifig count & character count by gender
+g1 <- ggplot(count_summarized, aes(x=type, y=value, fill=gender)) +
+  geom_col(position = "dodge") +
+  geom_text(aes(label=value),position = position_dodge(width = 1),vjust=-0.2) +
+  scale_fill_wbi() +
+  labs(title = "Star Wars Minifigs Count versus Character Count by Gender",
+       x="",
+       y="Count",
+       fill="Gender")
+g1
+
+# Bar chart to compare screentime by gender
+g2 <- ggplot(filter(screentime_summarized,type=="screentime_hours"), aes(x=gender, y=value, fill=gender)) +
+  geom_col(show.legend = FALSE) +
+  geom_text(aes(label=value), vjust=0) +
+  scale_fill_wbi() +
+  labs(title = "Star Wars Screentime Distribution by Gender",
+       x="Gender",
+       y="Screentime (hours)")
+g2
